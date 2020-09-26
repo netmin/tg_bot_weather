044cbe3bc9f2782f91ce13add54deb444a68cf4d
Vital D <admin@netmin.ru>
2020-09-26 11:34:52 +0500

    output few hours

--- app/handlers/user/weather.py
+++ app/handlers/user/weather.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 import aiohttp
 from aiogram import types
@@ -17,11 +18,33 @@ async def get_weather(message: types.Message):
                 if city['name'] == 'Yekaterinburg':
                     lon = city['coord']['lon']
                     lat = city['coord']['lat']
-        resp = await fetch(session, f'{base_url}lat={lat}&lon={lon}&units=metric&appid={app_id}')
+                    timezone = 'Asia/Yekaterinburg'
+                    timezone_offset = 5 * 60 * 60
+        resp = await fetch(session, f'{base_url}lat={lat}&lon={lon}&units=metric&timezone_offset={timezone_offset}&timezone={timezone}&appid={app_id}')
+
+        # current
         current = resp['current']
         temp = current['temp']
         feels_like = current['feels_like']
-        answer = f'Now {temp}C feels like {feels_like}C'
+        weather_list = current['weather']
+        weather_result = []
+        for weather in weather_list:
+            weather_result.append(weather['main'])
+
+        # hourly
+        hourly_list = resp['hourly']
+        hour_result = ''
+        for hour in hourly_list[1:9]:
+            local_time = hour['dt'] + timezone_offset
+            hour_temp = hour['temp']
+            hour_weather_list = hour['weather']
+            hour_weather_result = []
+            for hour_weather in hour_weather_list:
+                hour_weather_result.append(hour_weather['main'])
+            f_hour = datetime.utcfromtimestamp(local_time).strftime('%H:%M')
+            hour_result += f'{f_hour} - {hour_temp}c - {hour_weather_result}\n'
+
+        answer = f'Now {temp}c feels like {feels_like}c {weather_result} \n{hour_result}'
         await message.answer(answer)
 
 
